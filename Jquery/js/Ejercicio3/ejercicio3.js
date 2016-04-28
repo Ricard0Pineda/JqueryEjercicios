834ccaa63b8e51e4dd9b05fdb54f4e4dfac9b194
Ricardo Pineda <riquichy@gmail.com>
2016-04-28 17:57:07 -0500

    Se suben algunos cambios pequeños

---
+++ Jquery/js/Ejercicio3/ejercicio3.js
@@ -0,0 +1,31 @@
+$(document).on('ready', function () {
+	$('#c1').hover(function () {
+		$('#c1').css('background-color', 'blue');
+	});
+	
+	$('#c1').mouseleave(function () {
+		$('#c1').css('background', 'white');
+	});
+	$('#c2').hover(function () {
+		$('#c2').css('background-color', 'blue');
+	});
+	
+	$('#c2').mouseleave(function () {
+		$('#c2').css('background', 'white');
+	});
+	$('#c3').hover(function () {
+		$('#c3').css('background-color', 'blue');
+	});
+	
+	$('#c3').mouseleave(function () {
+		$('#c3').css('background', 'white');
+	});
+	$('#c4').hover(function () {
+		$('#c4').css('background-color', 'blue');
+	});
+	
+	$('#c4').mouseleave(function () {
+		$('#c4').css('background', 'white');
+	});
+	
+});
